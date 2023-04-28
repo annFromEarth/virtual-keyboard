17ece0c1f209da128ca1ede32dbb658d6d6d5485
annFromEarth <dv_anna@mail.ru>
2023-04-28 20:10:21 +0300

    feat: create keys source file, add favicon

---
+++ src/js/create_element.js
@@ -0,0 +1,2 @@
+const main = document.createElement('main');
+document.body.append(main);
---
+++ src/js/keys _en_ru_.js
@@ -0,0 +1,999 @@
+const keys = [
+  {
+
+    keyCode: 192, // row 5
+    en: {
+      name: '`',
+      value: '`',
+      shiftedValue: '~',
+      isCapsed: false,
+    },
+    ru: {
+      name: 'ё',
+      value: 'ё',
+      shiftedValue: 'Ё',
+      isCapsed: true,
+    },
+
+  },
+
+  {
+
+    keyCode: 49,
+    en: {
+      name: '1',
+      value: '1',
+      shiftedValue: '!',
+      isCapsed: false,
+    },
+    ru: {
+      name: '1',
+      value: '1',
+      shiftedValue: '!',
+      isCapsed: false,
+    },
+
+  },
+
+  {
+
+    keyCode: 50,
+    en: {
+      name: '2',
+      value: '2',
+      shiftedValue: '@',
+      isCapsed: false,
+    },
+    ru: {
+      name: '2',
+      value: '2',
+      shiftedValue: '"',
+      isCapsed: false,
+    },
+
+  },
+
+  {
+
+    keyCode: 51,
+    en: {
+      name: '3',
+      value: '3',
+      shiftedValue: '#',
+      isCapsed: false,
+    },
+    ru: {
+      name: '3',
+      value: '3',
+      shiftedValue: '№',
+      isCapsed: false,
+    },
+
+  },
+
+  {
+
+    keyCode: 52,
+    en: {
+      name: '4',
+      value: '4',
+      shiftedValue: '#',
+      isCapsed: false,
+    },
+    ru: {
+      name: '4',
+      value: '4',
+      shiftedValue: '№',
+      isCapsed: false,
+    },
+
+  },
+
+  {
+
+    keyCode: 53,
+    en: {
+      name: '5',
+      value: '5',
+      shiftedValue: '%',
+      isCapsed: false,
+    },
+    ru: {
+      name: '5',
+      value: '5',
+      shiftedValue: '%',
+      isCapsed: false,
+    },
+
+  },
+
+  {
+
+    keyCode: 54,
+    en: {
+      name: '6',
+      value: '6',
+      shiftedValue: '^',
+      isCapsed: false,
+    },
+    ru: {
+      name: '6',
+      value: '6',
+      shiftedValue: ':',
+      isCapsed: false,
+    },
+
+  },
+
+  {
+
+    keyCode: 55,
+    en: {
+      name: '7',
+      value: '7',
+      shiftedValue: '&',
+      isCapsed: false,
+    },
+    ru: {
+      name: '7',
+      value: '7',
+      shiftedValue: '?',
+      isCapsed: false,
+    },
+
+  },
+
+  {
+
+    keyCode: 56,
+    en: {
+      name: '8',
+      value: '8',
+      shiftedValue: '*',
+      isCapsed: false,
+    },
+    ru: {
+      name: '8',
+      value: '8',
+      shiftedValue: '*',
+      isCapsed: false,
+    },
+
+  },
+
+  {
+
+    keyCode: 57,
+    en: {
+      name: '9',
+      value: '9',
+      shiftedValue: '(',
+      isCapsed: false,
+    },
+    ru: {
+      name: '9',
+      value: '9',
+      shiftedValue: '(',
+      isCapsed: false,
+    },
+
+  },
+
+  {
+
+    keyCode: 48,
+    en: {
+      name: '0',
+      value: '0',
+      shiftedValue: ')',
+      isCapsed: false,
+    },
+    ru: {
+      name: '0',
+      value: '0',
+      siftedValue: ')',
+      isCapsed: false,
+    },
+
+  },
+
+  {
+
+    keyCode: 173,
+    en: {
+      name: '-',
+      value: '-',
+      shiftedValue: '_',
+      isCapsed: false,
+    },
+    ru: {
+      name: '-',
+      value: '-',
+      shiftedValue: '_',
+      isCapsed: false,
+    },
+
+  },
+
+  {
+
+    keyCode: 61,
+    en: {
+      name: '=',
+      value: '=',
+      shiftedValue: '+',
+      isCapsed: false,
+    },
+    ru: {
+      name: '=',
+      value: '=',
+      shiftedValue: '+',
+      isCapsed: false,
+    },
+
+  },
+
+  {
+
+    keyCode: 8,
+    en: {
+      name: 'backspace',
+    },
+    ru: {
+      name: 'backspace',
+    },
+
+  },
+
+  { // row 4
+
+    keyCode: 9,
+    en: {
+      name: 'tab',
+    },
+    ru: {
+      name: 'tab',
+    },
+
+  },
+
+  {
+
+    keyCode: 81,
+    en: {
+      name: 'q',
+      value: 'q',
+      shiftedValue: 'Q',
+      isCapsed: true,
+    },
+    ru: {
+      name: 'й',
+      value: 'й',
+      shiftedValue: 'Й',
+      isCapsed: true,
+    },
+
+  },
+
+  {
+
+    keyCode: 87,
+    en: {
+      name: 'w',
+      value: 'w',
+      shiftedValue: 'W',
+      isCapsed: true,
+    },
+    ru: {
+      name: 'ц',
+      value: 'ц',
+      shiftedValue: 'Ц',
+      isCapsed: true,
+    },
+
+  },
+
+  {
+
+    keyCode: 69,
+    en: {
+      name: 'e',
+      value: 'e',
+      shiftedValue: 'E',
+      isCapsed: true,
+    },
+    ru: {
+      name: 'у',
+      value: 'у',
+      shiftedValue: 'У',
+      isCapsed: true,
+    },
+
+  },
+
+  {
+
+    keyCode: 82,
+    en: {
+      name: 'r',
+      value: 'r',
+      shiftedValue: 'R',
+      isCapsed: true,
+    },
+    ru: {
+      name: 'к',
+      value: 'к',
+      shiftedValue: 'К',
+      isCapsed: true,
+    },
+
+  },
+
+  {
+
+    keyCode: 84,
+    en: {
+      name: 't',
+      value: 't',
+      shiftedValue: 'T',
+      isCapsed: true,
+    },
+    ru: {
+      name: 'е',
+      value: 'е',
+      shiftedValue: 'Е',
+      isCapsed: true,
+    },
+
+  },
+
+  {
+
+    keyCode: 89,
+    en: {
+      name: 'y',
+      value: 'y',
+      shiftedValue: 'Y',
+      isCapsed: true,
+    },
+    ru: {
+      name: 'н',
+      value: 'н',
+      shiftedValue: 'Н',
+      isCapsed: true,
+    },
+
+  },
+
+  {
+
+    keyCode: 85,
+    en: {
+      name: 'u',
+      value: 'u',
+      shiftedValue: 'U',
+      isCapsed: true,
+    },
+    ru: {
+      name: 'г',
+      value: 'г',
+      shiftedValue: 'Г',
+      isCapsed: true,
+    },
+
+  },
+
+  {
+
+    keyCode: 73,
+    en: {
+      name: 'i',
+      value: 'i',
+      shiftedValue: 'I',
+      isCapsed: true,
+    },
+    ru: {
+      name: 'ш',
+      value: 'ш',
+      shiftedValue: 'Ш',
+      isCapsed: true,
+    },
+
+  },
+
+  {
+
+    keyCode: 79,
+    en: {
+      name: 'o',
+      value: 'o',
+      shiftedValue: 'O',
+      isCapsed: true,
+    },
+    ru: {
+      name: 'щ',
+      value: 'щ',
+      shiftedValue: 'Щ',
+      isCapsed: true,
+    },
+
+  },
+
+  {
+
+    keyCode: 80,
+    en: {
+      name: 'p',
+      value: 'p',
+      shiftedValue: 'P',
+      isCapsed: true,
+    },
+    ru: {
+      name: 'з',
+      value: 'з',
+      shiftedValue: 'З',
+      isCapsed: true,
+    },
+
+  },
+
+  {
+
+    keyCode: 219,
+    en: {
+      name: '[',
+      value: '[',
+      shiftedValue: '{',
+      isCapsed: false,
+    },
+    ru: {
+      name: 'х',
+      value: 'х',
+      shiftedValue: 'Х',
+      isCapsed: true,
+    },
+
+  },
+
+  {
+
+    keyCode: 221,
+    en: {
+      name: ']',
+      value: ']',
+      shiftedValue: '}',
+      isCapsed: false,
+    },
+    ru: {
+      name: 'ъ',
+      value: 'ъ',
+      shiftedValue: 'Ъ',
+      isCapsed: true,
+    },
+
+  },
+
+  {
+
+    keyCode: 220,
+    en: {
+      name: '\\',
+      value: '\\',
+      shiftedValue: '|',
+      isCapsed: false,
+    },
+    ru: {
+      name: '\\',
+      value: '\\',
+      shiftedValue: '/',
+      isCapsed: false,
+    },
+
+  },
+
+  {
+
+    keyCode: 46,
+    name: 'delete',
+
+  },
+
+  { // row 3
+
+    keyCode: 20,
+    name: 'capslock',
+
+  },
+
+  {
+
+    keyCode: 65,
+    en: {
+      name: 'a',
+      value: 'a',
+      shiftedValue: 'A',
+      isCapsed: true,
+    },
+    ru: {
+      name: 'ф',
+      value: 'ф',
+      shiftedValue: 'Ф',
+      isCapsed: true,
+    },
+
+  },
+
+  {
+
+    keyCode: 83,
+    en: {
+      name: 's',
+      value: 's',
+      shiftedValue: 'S',
+      isCapsed: true,
+    },
+    ru: {
+      name: 'ы',
+      value: 'ы',
+      shiftedValue: 'Ы',
+      isCapsed: true,
+    },
+
+  },
+
+  {
+
+    keyCode: 68,
+    en: {
+      name: 'd',
+      value: 'd',
+      shiftedValue: 'D',
+      isCapsed: true,
+    },
+    ru: {
+      name: 'в',
+      value: 'в',
+      shiftedValue: 'В',
+      isCapsed: true,
+    },
+
+  },
+
+  {
+
+    keyCode: 70,
+    en: {
+      name: 'f',
+      value: 'f',
+      shiftedValue: 'F',
+      isCapsed: true,
+    },
+    ru: {
+      name: 'а',
+      value: 'а',
+      shiftedValue: 'А',
+      isCapsed: true,
+    },
+
+  },
+
+  {
+
+    keyCode: 71,
+    en: {
+      name: 'g',
+      value: 'g',
+      shiftedValue: 'G',
+      isCapsed: true,
+    },
+    ru: {
+      name: 'п',
+      value: 'п',
+      shiftedValue: 'П',
+      isCapsed: true,
+    },
+
+  },
+
+  {
+
+    keyCode: 72,
+    en: {
+      name: 'h',
+      value: 'h',
+      shiftedValue: 'H',
+      isCapsed: true,
+    },
+    ru: {
+      name: 'р',
+      value: 'р',
+      shiftedValue: 'Р',
+      isCapsed: true,
+    },
+
+  },
+
+  {
+
+    keyCode: 74,
+    en: {
+      name: 'j',
+      value: 'j',
+      shiftedValue: 'J',
+      isCapsed: true,
+    },
+    ru: {
+      name: 'о',
+      value: 'о',
+      shiftedValue: 'О',
+      isCapsed: true,
+    },
+
+  },
+
+  {
+
+    keyCode: 75,
+    en: {
+      name: 'k',
+      value: 'k',
+      shiftedValue: 'K',
+      isCapsed: true,
+    },
+    ru: {
+      name: 'л',
+      value: 'л',
+      shiftedValue: 'Л',
+      isCapsed: true,
+    },
+
+  },
+
+  {
+
+    keyCode: 76,
+    en: {
+      name: 'l',
+      value: 'l',
+      shiftedValue: 'L',
+      isCapsed: true,
+    },
+    ru: {
+      name: 'д',
+      value: 'д',
+      shiftedValue: 'Д',
+      isCapsed: true,
+    },
+
+  },
+
+  {
+
+    keyCode: 59,
+    en: {
+      name: ';',
+      value: ';',
+      shiftedValue: ':',
+      isCapsed: false,
+    },
+    ru: {
+      name: 'ж',
+      value: 'ж',
+      shiftedValue: 'Ж',
+      isCapsed: true,
+    },
+
+  },
+
+  {
+
+    keyCode: 222,
+    en: {
+      name: '\'',
+      value: '\'',
+      shiftedValue: '"',
+      isCapsed: false,
+    },
+    ru: {
+      name: 'э',
+      value: 'э',
+      shiftedValue: 'Э',
+      isCapsed: true,
+    },
+
+  },
+
+  {
+
+    keyCode: 13,
+    name: 'enter',
+    value: '&#8617;',
+    shiftedValue: '&#8617;',
+
+  },
+
+  { // row 2
+    keyCode: 16,
+    name: 'shift',
+    value: '&#8679;',
+    type: 'left',
+  },
+
+  {
+    keyCode: 90,
+    en: {
+      name: 'z',
+      value: 'z',
+      shiftedValue: 'Z',
+      isCapsed: true,
+    },
+    ru: {
+      name: 'я',
+      value: 'я',
+      shiftedValue: 'Я',
+      isCapsed: true,
+    },
+  },
+
+  {
+
+    keyCode: 88,
+    en: {
+      name: 'x',
+      value: 'x',
+      shiftedValue: 'X',
+      isCapsed: true,
+    },
+    ru: {
+      name: 'ч',
+      value: 'ч',
+      shiftedValue: 'Ч',
+      isCapsed: true,
+    },
+
+  },
+
+  {
+
+    keyCode: 67,
+    en: {
+      name: 'c',
+      value: 'c',
+      shiftedValue: 'C',
+      isCapsed: true,
+    },
+    ru: {
+      name: 'с',
+      value: 'с',
+      shiftedValue: 'С',
+      isCapsed: true,
+    },
+
+  },
+
+  {
+
+    keyCode: 86,
+    en: {
+      name: 'v',
+      value: 'v',
+      shiftedValue: 'V',
+      isCapsed: true,
+    },
+    ru: {
+      name: 'м',
+      value: 'м',
+      shiftedValue: 'М',
+      isCapsed: true,
+    },
+
+  },
+
+  {
+
+    keyCode: 66,
+    en: {
+      name: 'b',
+      value: 'b',
+      shiftedValue: 'B',
+      isCapsed: true,
+    },
+    ru: {
+      name: 'и',
+      value: 'и',
+      shiftedValue: 'И',
+      isCapsed: true,
+    },
+
+  },
+
+  {
+
+    keyCode: 78,
+    en: {
+      name: 'n',
+      value: 'n',
+      shiftedValue: 'N',
+      isCapsed: true,
+    },
+    ru: {
+      name: 'т',
+      value: 'т',
+      shiftedValue: 'Т',
+      isCapsed: true,
+    },
+
+  },
+
+  {
+
+    keyCode: 77,
+    en: {
+      name: 'm',
+      value: 'm',
+      shiftedValue: 'M',
+      isCapsed: true,
+    },
+    ru: {
+      name: 'ь',
+      value: 'ь',
+      shiftedValue: 'Ь',
+      isCapsed: true,
+    },
+
+  },
+
+  {
+
+    keyCode: 188,
+    en: {
+      name: ',',
+      value: ',',
+      shiftedValue: '<',
+      isCapsed: false,
+    },
+    ru: {
+      name: 'б',
+      value: 'б',
+      shiftedValue: 'Б',
+      isCapsed: true,
+    },
+
+  },
+
+  {
+
+    keyCode: 190,
+    en: {
+      name: '.',
+      value: '.',
+      shiftedValue: '>',
+      isCapsed: false,
+    },
+    ru: {
+      name: 'ю',
+      value: 'ю',
+      shiftedValue: 'Ю',
+      isCapsed: true,
+    },
+
+  },
+
+  {
+
+    keyCode: 191, // 190 remains for russian????
+    en: {
+      name: '/',
+      value: '/',
+      shiftedValue: '?',
+      isCapsed: false,
+    },
+    ru: {
+      name: '!',
+      value: '!',
+      shiftedValue: '§',
+      isCapsed: false,
+    },
+
+  },
+
+  {
+    keyCode: 38,
+    name: 'arrowUp',
+    value: '&#9652;',
+    shiftedValue: '&#9652;',
+  },
+
+  {
+    keyCode: 16,
+    name: 'shift',
+    value: '&#8679',
+    type: 'right',
+  },
+
+  { // row 1
+
+    keyCode: 17,
+    name: 'ctrl',
+    type: 'left',
+    value: '',
+    shiftedValue: '',
+    isCapsed: false,
+
+  },
+
+  {
+
+    keyCode: 91,
+    name: 'OS',
+    value: '',
+    shiftedValue: '',
+    isCapsed: false,
+
+  },
+
+  {
+
+    keyCode: 18,
+    name: 'alt',
+    type: 'left',
+    value: '',
+    shiftedValue: '',
+    isCapsed: false,
+
+  },
+
+  {
+
+    keyCode: 32,
+    name: 'whitespace',
+    value: '',
+    shiftedValue: '',
+    isCapsed: false,
+
+  },
+
+  {
+
+    keyCode: 18,
+    name: 'alt',
+    type: 'right',
+    value: '',
+    shiftedValue: '',
+    isCapsed: false,
+
+  },
+
+  {
+
+    keyCode: 17,
+    name: 'ctrl',
+    type: 'right',
+    value: '',
+    shiftedValue: '',
+    isCapsed: false,
+
+  },
+
+  {
+    keyCode: 37,
+    name: 'arrowLeft',
+    value: '&#9666;',
+    shiftedValue: '&#9666;',
+  },
+  {
+    keyCode: 40,
+    name: 'arrowDown',
+    value: '&#9662;',
+    shiftedValue: '&#9662;',
+  },
+  {
+    keyCode: 39,
+    name: 'arrowRight',
+    value: '&#9656;',
+    shiftedValue: '&#9656;',
+  },
+
+];
+
+export default keys;
